bacdf45af6c3db60e5035db1dd11b7fbf7476b1f
yadlapalli9999 <yadlapalli9999@gmail.com>
2022-10-05 19:40:08 +0530

    update room

--- pages/api/rooms/[id].js
+++ pages/api/rooms/[id].js
@@ -1,8 +1,10 @@
 import nc from 'next-connect';
-import { getSingleRoom } from '../../../controllers/roomController';
+import { getSingleRoom, updateRoom } from '../../../controllers/roomController';
 
 const handler = nc();
 
 handler.get(getSingleRoom)
+
+handler.put(updateRoom)
 
 export default handler;
