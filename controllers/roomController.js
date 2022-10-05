7bc04aebf4476f4d0425f52a735a07b5bc974d9c
yadlapalli9999 <yadlapalli9999@gmail.com>
2022-10-05 15:19:36 +0530

    creating new room and fecthing all rooms

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -1,10 +1,42 @@
+import Room from '../models/room';
 
+const allRooms = async(req,res)=>{
 
-const allRooms = (req,res)=>{
-   res.status(200).json({
-    success:true,
-    message:'All Rooms'
-   })
+    try{
+      let rooms = await Room.find()
+      res.status(200).json({
+        success:true,
+        count:rooms.length,
+        message:'All Rooms',
+        rooms
+       })
+    }
+    catch(error){
+      res.status(404).json({
+        success:false,
+        error:error.message
+      })
+    }
+   
 }
 
-export {allRooms}
+// create newRooms => /api/rooms
+
+const newRoom = async(req,res)=>{
+   try{
+     let room = await Room.create(req.body);
+     res.status(200).json({
+        success:true,
+        message:"Created new room successfully",
+        room
+     })
+   }
+   catch(error){
+    req.status(400).json({
+        success:false,
+        error:error.message
+    })
+   }
+}
+
+export {allRooms,newRoom}
