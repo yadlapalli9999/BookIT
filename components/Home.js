ba7e9b65bcb604a744b53cac646ccccfdb307193
yadlapalli9999 <yadlapalli9999@gmail.com>
2022-10-06 19:04:42 +0530

    dispatch rooms action in  getServerSideProps

--- components/Home.js
+++ components/Home.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Home = ()=>{
+
+    const {rooms} = useSelector(state=>state.allRooms)
+    console.log(rooms)
     return(
         <section id="rooms" className="container mt-5">
 
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Home from "../components/Home";
 import Layout from "../components/layout/Layout";
+import { getAllRooms } from "../redux/actions/roomActions";
+import { wrapper } from "../redux/store";
 
 export default function Index() {
   return (
@@ -8,3 +10,7 @@ export default function Index() {
    </Layout>
   )
 }
+
+export const getServerSideProps = wrapper.getServerSideProps(store =>  async({req})=>{
+   await store.dispatch(getAllRooms(req))
+})
