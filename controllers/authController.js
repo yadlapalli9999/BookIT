5e7b9d3ceddf5dc9ce825fa12d080c1bc7c22e8a
yadlapalli9999 <yadlapalli9999@gmail.com>
2022-10-10 21:21:56 +0530

    Adding cloudinary for images

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,23 @@
 import catchAsyncError from '../middlewares/catchAsyncError';
 import User from '../models/user';
 import ErrorHandler from '../utils/errorHandler';
+import cloudinary from 'cloudinary'
+
+
+//setting up cloudinary config
+cloudinary.config({
+  cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+  api_key:process.env.CLOUDINARY_API_KEY,
+  api_secret:process.env.CLOUDINARY_SECRET_KEY
+})
 
 //Register User => /api/auth/register
 const registerUser = catchAsyncError(async (req,res)=>{
+    const result = await cloudinary.v2.uploader.upload(req.body.avatar,{
+        folder:'bookit/avatars',
+        width:'150',
+        crop:'scale'
+    })
      const {name,email,password} = req.body;
 
      const user = await User.create({
@@ -11,8 +25,8 @@ const registerUser = catchAsyncError(async (req,res)=>{
         email,
         password,
         avatar:{
-            public_id:'PUBLIC_ID',
-            url:'URL'
+            public_id:result.public_id,
+            url:result.secure_url
         }
      })
 
--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env:{
-    DB_LOCAL_URI : 'mongodb://localhost:27017/bookit'
+    DB_LOCAL_URI : 'mongodb://localhost:27017/bookit',
+    CLOUDINARY_CLOUD_NAME:'dc26muxal',
+    CLOUDINARY_API_KEY:'943686368841334',
+    CLOUDINARY_SECRET_KEY:'oP--Se49moFEoLovdQv_Dt-ky54'
   },
   images: {
     domains: ['res.cloudinary.com'],
