07a294f30aab9b50a701254ef3d0610661cfbf7f
yadlapalli9999 <yadlapalli9999@gmail.com>
2022-10-11 16:58:44 +0530

    logout for front end functionality

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import Link from 'next/link';
 import { useDispatch, useSelector } from 'react-redux';
 import { loadUser } from '../../redux/actions/userActions';
+import { signOut } from 'next-auth/react';
 
 const Header = ()=>{
     let dispatch = useDispatch();
@@ -10,6 +11,10 @@ const Header = ()=>{
     useEffect(()=>{
       dispatch(loadUser())
     },[dispatch])
+
+    const logoutHandler = ()=>{
+       signOut()
+    }
     return(
         <nav className="navbar row justify-content-center sticky-top">
         <div className="container">
@@ -48,7 +53,7 @@ const Header = ()=>{
                                 </Link>
 
                                 <Link href='/'>
-                                    <a className="dropdown-item text-danger">Logout</a>
+                                    <a className="dropdown-item text-danger" onClick={logoutHandler}>Logout</a>
                                 </Link>
                             </div>
           </div>):(!loading &&  <Link href="/login">
